64cbe4908ba8295f0da601479663d9ce149f6e9c
Vadim Uzhakin <vadimuzhakin@gmail.com>
2022-01-22 19:48:48 +0700

    add spotbug

--- src/main/java/ru/netology/statistic_second/StatisticService.java
+++ src/main/java/ru/netology/statistic_second/StatisticService.java
@@ -10,12 +10,9 @@ public class StatisticService {
     public long findMax(long[] incomes) {
         long current_max_index = 0;
         long current_max = incomes[0];
-        for (long income : incomes) {
-            if (current_max < income) {
+        for (long income : incomes)
+            if (current_max < income)
                 current_max = income;
-            }
-            current_max_index++;
-        }
         return current_max;
 
     }
